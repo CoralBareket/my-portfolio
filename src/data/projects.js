c8459046fa7dbc69a64bf90cf06d96b92b9f7d6d
coral <coral.bareket@gmail.com>
2024-09-21 23:13:22 +0300

    Uploaded other project of mine

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,21 @@
-import memoryGameImage from '../assets/images/memoryGame.png';
 import onlineCodingImage from '../assets/images/onlineCoding.png';
+import petsAdoptionImage from '../assets/images/petsAdoption.png';
 
 const projects = [
   {
     id: 1,
-    title: 'Memory Game',
-    description: 'A classic memory game built with JavaScript, HTML, and CSS.',
-    link: 'https://github.com/CoralBareket/Memory-game',
-    image: memoryGameImage,
-  },
-  {
-    id: 2,
     title: 'Online Coding Site',
     description: 'An interactive coding site for learning and practicing programming.',
     link: 'https://github.com/CoralBareket/online-coding-webapp',
-    image: onlineCodingImage,  // Use the single image property
+    image: onlineCodingImage,  
   },
-  // Add more projects as needed
+  {
+    id: 2,
+    title: 'Pet Adoption Site',
+    description: '"Pawfect Match" is a pet adoption platform that matches potential adopters with dogs based on lifestyle, preferences, and various criteria to ensure the best fit for both the pet and the owner.',
+    link: 'https://github.com/CoralBareket/pets-adoption-webapp',
+    image: petsAdoptionImage,  
+  }
 ];
 
 export default projects;
